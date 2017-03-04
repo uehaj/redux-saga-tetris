0878857dac547ef6f52374f545e1f0de046d5b17
@uehaj <uehaj@jggug.org>
2017-03-04 10:54:21 +0900

    integrate redux-saga

---
+++ store.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import createLogger from 'redux-logger';
+import * as Types from './types';
+import rootSaga from './sagas';
+
+const initialState = {
+  counter: 0
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case Types.UI_BUTTON_CLICKED: {
+      return {
+        ...state,
+        counter: state.count + 1
+      };
+    };
+    default: return state;
+  }
+};
+
+export const initStore = (initialState) => {
+  const sagaMiddleware = createSagaMiddleware();
+  const logger = createLogger();
+  const store = createStore(reducer, initialState, applyMiddleware(sagaMiddleware, logger));
+  sagaMiddleware.run(rootSaga);
+  return store;
+}
